bb501f2165522611ab0bb375a9b5a662bf1ca348
sebnorth <sebnorth@gmail.com>
2020-04-26 11:55:28 +0200

    Dopisaliśmy pola i właściwości w klasie Samochód

---
+++ Zajecia1/Samochod.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zajecia1
+{
+    class Samochod
+    {
+        private string marka;
+
+        public string Marka
+        {
+            get { return marka; }
+            set { marka = value; }
+        }
+
+        private string model;
+
+        public string Model
+        {
+            get { return model; }
+            set { model = value; }
+        }
+
+        private int iloscDrzwi;
+
+        public int IloscDrzwi
+        {
+            get { return iloscDrzwi; }
+            set { iloscDrzwi = value; }
+        }
+
+        private int pojemnoscSilnika;
+
+        public int PojemnoscSilnika
+        {
+            get { return pojemnoscSilnika; }
+            set { pojemnoscSilnika = value; }
+        }
+
+        private double srednieSpalanie;
+
+        public double SrednieSpalanie
+        {
+            get { return srednieSpalanie; }
+            set { srednieSpalanie = value; }
+        }
+
+
+
+
+    }
+}
